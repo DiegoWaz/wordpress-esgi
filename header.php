85a56555a639035cbe9baeff66a4a593cf6f081e
Diego <waziri.diego@gmail.com>
2016-11-04 17:07:15 +0100

    first commit

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		<?php
+			wp_title();
+			echo " | ";
+			bloginfo('name');
+		?>
+	</title>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+
